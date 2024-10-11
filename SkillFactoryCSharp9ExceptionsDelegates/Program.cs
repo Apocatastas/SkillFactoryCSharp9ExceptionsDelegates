7e68822f90706f6d706bd07b4ab56e181f7ae480
ɐllɐ <molchanova.a.a@gmail.com>
2024-10-11 19:07:39 +0300

    task 9.1.3

--- SkillFactoryCSharp9ExceptionsDelegates/Program.cs
+++ SkillFactoryCSharp9ExceptionsDelegates/Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ExceptionsDelegates
 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Exception exception = new Exception();
+            exception.Data.Add("Создано: ", DateTime.Now);
+        }
+    }
+}
